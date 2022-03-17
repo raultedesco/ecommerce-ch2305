190e0e7c5726ae48fa21270358a37eb6952cb171
Raul Tedesco <raul.a.tedesco@gmail.com>
2022-03-16 21:20:35 -0300

    fix port

--- server.js
+++ server.js
@@ -99,7 +99,7 @@ app.use(mensajeRouter)
 app.use(apiErrorHandler)
 
 
-const PORT = process.env.PORT || 8081
+const PORT = process.env.PORT || 5000
 
 httpServer.listen(PORT, function () {
   logger.info(`App listening on ${PORT}`)
